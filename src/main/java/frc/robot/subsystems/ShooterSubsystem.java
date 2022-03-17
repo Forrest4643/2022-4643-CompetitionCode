a5b540e5b189bc04f2b32e68bb8df6f3f5ea12ab
Forrest4643 <90162968+Forrest4643@users.noreply.github.com>
2022-03-17 14:43:36 -0700

    commit on master

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;
+
+public class ShooterSubsystem extends SubsystemBase {
+  private final CANSparkMax leftMotor = new CANSparkMax(ShooterConstants.leftMotorID, MotorType.kBrushless);
+  private final CANSparkMax rightMotor = new CANSparkMax(ShooterConstants.rightMotorID, MotorType.kBrushless);
+
+  private final RelativeEncoder leftEncoder = leftMotor.getEncoder();
+  private final RelativeEncoder rightEncoder = rightMotor.getEncoder();
+
+  
+  /** Creates a new ShooterSubsystem. */
+  public ShooterSubsystem() {
+    leftMotor.setInverted(true);
+    rightMotor.setInverted(false);
+
+    leftEncoder.setVelocityConversionFactor(1.5);
+    rightEncoder.setVelocityConversionFactor(1.5);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void setShooterSpeed(double speed) {
+    leftMotor.set(speed);
+    rightMotor.set(speed);
+  }
+
+  public double shooterRPM() {
+    return (leftEncoder.getVelocity() + rightEncoder.getVelocity()) / 2;
+  }
+}
